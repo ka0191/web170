bdb1bff10af548897de4db64c54bf41d11426592
ka0191 <sunga0191@gmail.com>
2017-05-05 17:18:54 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,11 +3,16 @@
 <?php get_header(); ?>
 <!--end header.php-->
 
-<div id="bg">
-<div id="header">
-    <h1>227WORLD</h1>
-    <a href="products.php">SEE PRODUCTS</a>
-</div>
+<!--start content -->
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // replaced the ({) for (:)?>
+    <article id="article- <?php the_ID(); ?>" class="article">
+    <h2><a href="<?php the_permalink(); // it will link to the posting ?>"><?php the_title(); // it will get the posting title ?></a></h2>
+    <?php the_content(''); // it will get posting content ?>
+    </article>
+    <?php endwhile; endif; ?>
+    <?php if(is_404()) {echo 'Nothing was found.';}?>
+    <small>index.php</small>
+<!--end content-->
 
 
 <!--start footer.php-->
