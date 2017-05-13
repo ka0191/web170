6c435cd64067ad6d8638f3cd87f3b02e20199555
ka0191 <sunga0191@gmail.com>
2017-05-13 00:26:36 -0700

    Add files via upload
    06) Create Your Main & Sub Menu Systems

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+<aside id="sidebar">
+<?php
+echo '<h2>';
+if (is_page()) {
+	echo get_the_title($post->post_parent);
+} else {
+		echo 'Blog';
+	}
+	echo '</h2>';
+	echo '<ul>';
+	if (is_page() || is_404()) {
+		if ($post->post_parent) {
+			wp_list_pages(array('title_li' => '', 'child_of' => $post->post_parent,));
+		} else {
+			wp_list_pages(array('title_li' => '', 'child_of' => $post->ID,));
+		}
+	} else {
+		 wp_list_categories(array('title_li' => '',)); 
+		}
+		echo '</ul>';
+?>
+</aside>
